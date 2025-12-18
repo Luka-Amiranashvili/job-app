d1a443d4400a522aa716ae7ebd1fc99b36980bfa
Luka <luka.amiranashvili.2001@gmail.com>
2025-12-18 18:39:47 +0400

    feat: updated imports

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,21 @@
-const dotenv = require("dotenv");
-const express = require("express");
-const cors = require("cors");
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/auth/auth.routes";
+import connectDB from "./config/db";
 
 dotenv.config();
 
 const app = express();
+connectDB();
 const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.post("/test-me", (req, res) => {
+  res.json({ message: "Post requresd reached", body: req.body });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
