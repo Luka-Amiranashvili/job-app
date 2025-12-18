a05955a565cbccd394b6201e1ca884f39a8b3ac5
Luka <luka.amiranashvili.2001@gmail.com>
2025-12-18 18:50:01 +0400

    feat: created login auth & controler

--- backend/src/routes/auth/auth.routes.ts
+++ backend/src/routes/auth/auth.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { register } from "../../controllers/auth/auth.controller";
+import { login } from "../../controllers/auth/auth.controller";
 
 const router = Router();
 router.post("/register", register);
+router.post("/login", login);
 
 export default router;
