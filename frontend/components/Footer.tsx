3d1d4055ebd4cbef47b0fed0f66c0646430ad8b4
Luka <luka.amiranashvili.2001@gmail.com>
2025-12-20 13:55:44 +0400

    style: changed bg style for footer

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-white  py-12 border-t border-[#f0f2f4] dark:border-gray-800 mt-auto text-center">
+    <footer className="bg-white bg-[#f6f6f8] dark:bg-[#101622] text-[#111318] py-12 border-t border-[#f0f2f4] dark:border-gray-800 mt-auto text-center">
       <p className="text-[#616f89] text-sm">
         © 2025 JobBoard Inc. All rights reserved.
       </p>
