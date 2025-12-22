05cb1414696dde346b5b855b905ea3d1e585e19a
Luka <luka.amiranashvili.2001@gmail.com>
2025-12-22 18:11:31 +0400

    chore: installed ratelimit, helmet, hpp packages

--- backend/src/routes/auth/auth.routes.ts
+++ backend/src/routes/auth/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { register } from "../../controllers/auth/auth.controller";
 import { login } from "../../controllers/auth/auth.controller";
+import loginLimiter from "../../utils/rateLimit";
 
 const router = Router();
 router.post("/register", register);
-router.post("/login", login);
+router.post("/login", loginLimiter, login);
 
 export default router;
