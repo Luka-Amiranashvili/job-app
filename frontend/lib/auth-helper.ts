87e1ffd49835bd6dbd109c8bc48d168d71b46f80
Luka <luka.amiranashvili.2001@gmail.com>
2025-12-22 14:45:16 +0400

    feat: updated action for employers

--- frontend/lib/auth-helper.ts
+++ frontend/lib/auth-helper.ts
@@ -3,15 +3,28 @@
 import Cookies from "js-cookie";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
+export interface User {
+  name: string;
+  userType: string;
+  role?: "job_seeker" | "employer";
+}
+
 export const protectAction = (
   router: AppRouterInstance,
-  action: () => void
+  user: User | null,
+  action: () => void,
+  forEmployerOnly: boolean = false
 ) => {
   const token = Cookies.get("token");
 
   if (!token) {
     alert("Please log in to continue");
     router.push("/login");
+    return;
+  }
+
+  if (forEmployerOnly && user?.role !== "employer") {
+    alert("Only employers can perform this action.");
     return;
   }
 
